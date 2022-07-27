f415f419279db80a3214be416cf26d51bca5ba8c
Coburn Berry <coburn@compound.finance>
2022-07-27 12:09:15 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
