25f43eac1fb31cb0dbdad31f872d72c700fcfa60
Geoffrey Hayes <hayesgm@gmail.com>
2022-08-04 14:47:27 -0700

    Make CloudKMS truly optional, bump to 0.1.6

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+* 0.1.6 - Make CloudKMS truly optional
+* 0.1.5 - Add build-in Solidity error decoding, fix warnings, bump to 0.1.5
+* 0.1.4 - Memoize versus preload address for signers
+* 0.1.3 - Add RPC option for timeout, increase default to 30s
+* 0.1.2
+  - Include a built-in Erc20 contract implementation
+  - Allow 0 to specify not including an EIP-155 signature
+  - Add a simple contract address translation function
+* 0.1.1 - Add Ethereum Keypair Generation
+* 0.1.0 - Initial version
--- lib/signet/signer/cloud_kms.ex
+++ lib/signet/signer/cloud_kms.ex
@@ -1,85 +1,87 @@
-defmodule Signet.Signer.CloudKMS do
-  @moduledoc """
-  Signer to sign messages from a Google Cloud KMS key.
-  """
-  import Signet.Hash, only: [keccak: 1]
+if Code.ensure_loaded?(GoogleApi.CloudKMS.V1.Api.Projects) do
+  defmodule Signet.Signer.CloudKMS do
+    @moduledoc """
+    Signer to sign messages from a Google Cloud KMS key.
+    """
+    import Signet.Hash, only: [keccak: 1]
 
-  alias GoogleApi.CloudKMS.V1.Api.Projects, as: CloudKMSApi
+    alias GoogleApi.CloudKMS.V1.Api.Projects, as: CloudKMSApi
 
-  @doc ~S"""
-  Get the Ethereum address associated with the given KMS key version.
+    @doc ~S"""
+    Get the Ethereum address associated with the given KMS key version.
 
-  ## Examples
+    ## Examples
 
-      iex> {:ok, address} = Signet.Signer.CloudKMS.get_address("token", "project", "location", "keychain", "key", "version")
-      iex> Base.encode16(address)
-      "DDA641B2A76A4A7C3617815BB13281DD207B74D5"
-  """
-  @spec get_address(term(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
-          {:ok, binary()} | {:error, String.t()}
-  def get_address(cred, project, location, keychain, key, version) do
-    with {:ok, %GoogleApi.CloudKMS.V1.Model.PublicKey{algorithm: algorithm, pem: pem}} <-
-           CloudKMSApi.cloudkms_projects_locations_key_rings_crypto_keys_crypto_key_versions_get_public_key(
-             client(cred),
-             project,
-             location,
-             keychain,
-             key,
-             version
-           ) do
-      case algorithm do
-        "EC_SIGN_SECP256K1_SHA256" ->
-          [certs] = :public_key.pem_decode(pem)
-          {{:ECPoint, public_key}, _} = :public_key.pem_entry_decode(certs)
-          {:ok, Signet.Util.get_eth_address(public_key)}
+        iex> {:ok, address} = Signet.Signer.CloudKMS.get_address("token", "project", "location", "keychain", "key", "version")
+        iex> Base.encode16(address)
+        "DDA641B2A76A4A7C3617815BB13281DD207B74D5"
+    """
+    @spec get_address(term(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
+            {:ok, binary()} | {:error, String.t()}
+    def get_address(cred, project, location, keychain, key, version) do
+      with {:ok, %GoogleApi.CloudKMS.V1.Model.PublicKey{algorithm: algorithm, pem: pem}} <-
+             CloudKMSApi.cloudkms_projects_locations_key_rings_crypto_keys_crypto_key_versions_get_public_key(
+               client(cred),
+               project,
+               location,
+               keychain,
+               key,
+               version
+             ) do
+        case algorithm do
+          "EC_SIGN_SECP256K1_SHA256" ->
+            [certs] = :public_key.pem_decode(pem)
+            {{:ECPoint, public_key}, _} = :public_key.pem_entry_decode(certs)
+            {:ok, Signet.Util.get_eth_address(public_key)}
 
-        _ ->
-          {:error, "Invalid algorithm: #{algorithm}"}
+          _ ->
+            {:error, "Invalid algorithm: #{algorithm}"}
+        end
       end
     end
-  end
 
-  @doc ~S"""
-  Signs a message with the given KMS key version, after digesting the message with keccak.
+    @doc ~S"""
+    Signs a message with the given KMS key version, after digesting the message with keccak.
 
-  ## Examples
+    ## Examples
 
-      iex> {:ok, sig} = Signet.Signer.CloudKMS.sign("test", "token", "project", "location", "keychain", "key", "version")
-      iex> {:ok, recid} = Signet.Recover.find_recid("test", sig, Base.decode16!("DDA641B2A76A4A7C3617815BB13281DD207B74D5"))
-      iex> Signet.Recover.recover_eth("test", %{sig|recid: recid}) |> Base.encode16()
-      "DDA641B2A76A4A7C3617815BB13281DD207B74D5"
-  """
-  @spec sign(String.t(), term(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
-          {:ok, Curvy.Signature.t()} | {:error, String.t()}
-  def sign(message, cred, project, location, keychain, key, version) when is_binary(message) do
-    message_hash_enc =
-      message
-      |> keccak()
-      |> Base.encode64()
+        iex> {:ok, sig} = Signet.Signer.CloudKMS.sign("test", "token", "project", "location", "keychain", "key", "version")
+        iex> {:ok, recid} = Signet.Recover.find_recid("test", sig, Base.decode16!("DDA641B2A76A4A7C3617815BB13281DD207B74D5"))
+        iex> Signet.Recover.recover_eth("test", %{sig|recid: recid}) |> Base.encode16()
+        "DDA641B2A76A4A7C3617815BB13281DD207B74D5"
+    """
+    @spec sign(String.t(), term(), String.t(), String.t(), String.t(), String.t(), String.t()) ::
+            {:ok, Curvy.Signature.t()} | {:error, String.t()}
+    def sign(message, cred, project, location, keychain, key, version) when is_binary(message) do
+      message_hash_enc =
+        message
+        |> keccak()
+        |> Base.encode64()
 
-    with {:ok, response} <-
-           CloudKMSApi.cloudkms_projects_locations_key_rings_crypto_keys_crypto_key_versions_asymmetric_sign(
-             client(cred),
-             project,
-             location,
-             keychain,
-             key,
-             version,
-             body: %{
-               digest: %{
-                 sha256: message_hash_enc
+      with {:ok, response} <-
+             CloudKMSApi.cloudkms_projects_locations_key_rings_crypto_keys_crypto_key_versions_asymmetric_sign(
+               client(cred),
+               project,
+               location,
+               keychain,
+               key,
+               version,
+               body: %{
+                 digest: %{
+                   sha256: message_hash_enc
+                 }
                }
-             }
-           ),
-         {:ok, decoded_sig} <- Base.decode64(response.signature) do
-      {:ok, Curvy.Signature.parse(decoded_sig)}
+             ),
+           {:ok, decoded_sig} <- Base.decode64(response.signature) do
+        {:ok, Curvy.Signature.parse(decoded_sig)}
+      end
     end
-  end
 
-  defp client(token) when is_binary(token), do: GoogleApi.CloudKMS.V1.Connection.new(token)
+    defp client(token) when is_binary(token), do: GoogleApi.CloudKMS.V1.Connection.new(token)
 
-  defp client(cred) do
-    %{token: token, type: "Bearer"} = Goth.fetch!(cred)
-    GoogleApi.CloudKMS.V1.Connection.new(token)
+    defp client(cred) do
+      %{token: token, type: "Bearer"} = Goth.fetch!(cred)
+      GoogleApi.CloudKMS.V1.Connection.new(token)
+    end
   end
 end
