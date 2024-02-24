f2f5130107da961d6d35a4a2c92ffc7d6869397f
Geoffrey Hayes <hayesgm@gmail.com>
2024-02-23 16:37:59 -0800

    Allow Wide V2 Signatures
    Due to EIP-155, some signatures come in wide (e.g. with a very large recovery "bit"), this is esp. true for nets like Sepolia with chain_id=11155111. Signatures should be passed in in a nice parsed way (r, s, and v), but if they are binary-packed, then EIP-155 means that large nets will overflow 1-byte (originally allocated for `v`). Here, we expand the signature to allow wider-`v`s.
    
    Bump to 1.0.0-beta3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "1.0.0-beta2",
+      version: "1.0.0-beta3",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
