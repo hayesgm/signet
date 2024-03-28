e709a46cf83612d2e60284635ae07ddd1d2bccfa
Scott Silver <scott@compound.finance>
2024-03-28 12:49:27 -0700

    bump ABI to alpha9; update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "1.0.0-charlie8",
+      version: "1.0.0-charlie9",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -52,7 +52,7 @@ defmodule Signet.MixProject do
       {:curvy, "~> 0.3.1"},
       {:goth, "~> 1.4.3", optional: true},
       {:ex_rlp, "~> 0.6.0"},
-      {:abi, "~> 1.0.0-alpha8"},
+      {:abi, "~> 1.0.0-alpha9"},
       {:junit_formatter, "~> 3.3.1", only: [:test]}
     ]
   end
