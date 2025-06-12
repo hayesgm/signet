c5e1cd9f55606328e3df36af91ccd7e969f6e453
Coburn Berry <32463466+coburncoburn@users.noreply.github.com>
2025-06-12 11:54:14 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "1.3.8",
+      version: "1.3.9",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
