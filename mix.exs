9b1dc0e000d2b90f0f6dded1e5f2186fd2316ce2
fluffywaffles <skorlir@gmail.com>
2025-11-25 12:07:35 -0800

    chore: bump to 1.4.0: mildly incompatible changes wrt. error types

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "1.3.12",
+      version: "1.4.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
