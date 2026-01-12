7860d6aa1d6c9afa744bff410a780e4024a47e12
Geoffrey Hayes <hayesgm@gmail.com>
2026-01-12 13:05:44 -0800

    Add Sign Digest. Bump 1.5.0.
    This patch adds the ability to sign a pre-digest message, at least via
    the Curvy signer.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Signet.MixProject do
   def project do
     [
       app: :signet,
-      version: "1.4.3",
+      version: "1.5.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
