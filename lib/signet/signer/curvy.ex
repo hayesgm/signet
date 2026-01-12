5bf40d4b76cb05154bbf13cb5b683e3ae0d794fa
Geoffrey Hayes <hayesgm@gmail.com>
2026-01-12 13:05:44 -0800

    Add Sign Digest. Bump 1.5.0.
    This patch adds the ability to sign a pre-digest message, at least via
    the Curvy signer.

--- lib/signet/signer/curvy.ex
+++ lib/signet/signer/curvy.ex
@@ -1,6 +1,6 @@
 defmodule Signet.Signer.Curvy do
   @moduledoc """
-  Signer to sign messages using a private key directly. 
+  Signer to sign messages using a private key directly.
 
   Note: this should not be used in production systems. Please see `Signet.Signer.CloudKMS`.
   """
@@ -39,9 +39,25 @@ defmodule Signet.Signer.Curvy do
   """
   @spec sign(String.t(), binary()) :: {:ok, Curvy.Signature.t()} | {:error, String.t()}
   def sign(message, private_key) when is_binary(message) do
-    priv_key = Curvy.Key.from_privkey(private_key)
+    sign_digest(keccak(message), private_key)
+  end
 
-    message_hash = keccak(message)
+  @doc ~S"""
+  Signs the given message which was already digested.
+
+  ## Examples
+
+      iex> use Signet.Hex
+      iex> priv_key = ~h[0x800509fa3e80882ad0be77c27505bdc91380f800d51ed80897d22f9fcc75f4bf]
+      iex> message_hash = ~h[0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658]
+      iex> {:ok, sig} = Signet.Signer.Curvy.sign_digest(message_hash, priv_key)
+      iex> {:ok, recid} = Signet.Recover.find_recid("test", sig, ~h[0x63Cc7c25e0cdb121aBb0fE477a6b9901889F99A7])
+      iex> Signet.Recover.recover_eth("test", %{sig|recid: recid}) |> Signet.Hex.to_address()
+      "0x63Cc7c25e0cdb121aBb0fE477a6b9901889F99A7"
+  """
+  @spec sign_digest(String.t(), binary()) :: {:ok, Curvy.Signature.t()} | {:error, String.t()}
+  def sign_digest(message_hash, private_key) when is_binary(message_hash) do
+    priv_key = Curvy.Key.from_privkey(private_key)
 
     Curvy.sign(message_hash, priv_key, hash: :keccak)
     |> Curvy.Signature.parse()
